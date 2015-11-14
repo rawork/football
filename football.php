166070320dd232aa4ce62dd092c31e6c93a4ae88
Roman Alyakritskiy <rawork@yandex.ru>
2015-11-14 19:35:46 +0300

    new code

--- football.php
+++ football.php
@@ -6,7 +6,7 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="icon" type="image/png" href="img/football.png" />
     <link type="text/css" rel="stylesheet" href="css/jcarousel.css?2015111204" media="screen">
-    <link type="text/css" rel="stylesheet" href="css/style.css?2015111305" media="screen">
+    <link type="text/css" rel="stylesheet" href="css/style.css?2015111401" media="screen">
 </head>
 <body>
     <div class="container">
@@ -15,46 +15,94 @@
             <div class="title">АНО Организационный комитет<br>«Россия-2018»</div>
         </div>
         <div class="content">
+            <div class="prolog clearfix">
+                <div class="col-5 text-left text-normal">
+                    <h2>Уважаемые члены АНО «Оргкомитет «Россия-2018»!</h2>
+                    <p>Кадровый холдинг АНКОР выражает вам свое уважение и благодарит вас за
+                        приглашение к тендеру на оказание услуг по поиску и подбору постоянного
+                        персонала и предоставлению специалистов в сфере маркетинга рекрутмента.
+                        Мы высоко ценим возможность принять участие в событии
+                        межгосударственного масштаба и уверены, что многолетний опыт реализации
+                        амбициозных проектов для российского и международного бизнеса, а также
+                        наша инфраструктура и безупречно профессиональная команда будут
+                        способствовать достижению ваших целей в области управления человеческими
+                        ресурсами.</p>
+                </div>
+                <ul class="video">
+                    <li class="text-bold">
+                        Сергей Саликов
+                        <iframe width="100%" height="160" src="https://www.youtube.com/embed/OTcns0LakBg?rel=0" frameborder="0" allowfullscreen></iframe>
+                    </li>
+                    <li class="text-bold">
+                        Алексей Миронов
+                        <iframe width="100%" height="160" src="https://www.youtube.com/embed/uLYlmyVWQ4k?rel=0" frameborder="0" allowfullscreen></iframe>
+                    </li>
+                    <li class="text-bold">
+                        Анастасия Теплоухова
+                        <iframe width="100%" height="160" src="https://www.youtube.com/embed/8QFhZeQKGjE?rel=0" frameborder="0" allowfullscreen></iframe>
+                    </li>
+                </ul>
+            </div>
+            <ul class="docs">
+                <li><a target="_blank" href="files/project_team_portfolio.pdf">Команда проекта</a></li>
+                <li>|</li>
+                <li><a target="_blank" href="files/resource_evaluation.pdf">Оценка ресурсов</a></li>
+                <li>|</li>
+                <li><a target="_blank" href="files/technology_and_metodology.pdf">Технология и методология</a></li>
+                <li>|</li>
+                <li><a target="_blank" href="files/">Управление проектом и взаимодействие</a></li>
+            </ul>
+            <div class="text">
+
+            </div>
+            <div class="graph">
+                <img src="img/infographic.png">
+                <div class="text-right"><a target="_blank" href="files/ancor_about.pdf">Подробнее</a></div>
+            </div>
+
             <div class="row">
                 <div class="col col-6">
-                    <h1>Команда АНКОРа. Основной состав</h1>
+                    <h1>Команда АНКОРа</h1>
                 </div>
                 <div class="col col-4 jcarousel">
                     <ul class="team">
                         <li class="member">
-                            <a href="" data-phone="+7 111 222 33 44" data-email="a.mironov@ancor.ru"><img src="img/people/Salikov.jpg"></a>
+                            <img src="img/people/Salikov.jpg">
+                            <div class="football-position">Главный менеджер</div>
                             <div class="name">Сергей Саликов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
-                            <div class="description">
-                                <p><b>В апреле 2011 г. назначен директором по стратегическому развитию бизнеса холдинга АНКОР.</b></p>
-                                <p><b>в АНКОРе: </b>в 2005 г. начал работать в АНКОРе на позиции национального менеджера по развитию бизнеса, а позже возглавил отдел по управлению ключевыми клиентами. С 11 января 2009 г. занимал должность директора по работе с клиентами кадрового холдинга АНКОР. 11 января 2010 г. назначен заместителем генерального директора по стратегическому развитию бизнеса холдинга АНКОР, <span>а с&nbsp;апреля 2011 года является директором по&nbsp;стратегическому развитию АНКОРа</span>.</p>
-                                <p> <b>Карьера:</b> в 2001-2005 гг. работал в компании Coleman Services, в которой прошел путь от консультанта до руководителя отдела маркетинга и развития бизнеса. </p>
-                                <p><b> Образование: </b>окончил Коломенский государственный педагогический институт, имеет два диплома по специальности «Лингвистика» и «Национальная экономика». Также получил степень MBA по специальности «Маркетинг» во Всероссийской академии внешней торговли. </p>
+                            <div class="position">Генеральный директор холдинга АНКОР<br>
+                                21год опыта работы в АНКОРе</div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/people/Mironov.jpg"></a>
+                            <img src="img/people/Mironov.jpg">
+                            <div class="football-position">Главный тренер</div>
                             <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <div class="position">Директор по стратегии холдинга АНКОР<br>
+                                Управляющий Директор ANCOR Professional<br>
+                                10 лет опыта работы в АНКОРе</div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <img src="img/member.jpg">
+                            <div class="football-position">Капитан команды</div>
+                            <div class="name">Анастасия Теплоухова</div>
+                            <div class="position">Исполнительный директор ANCOR Professional<br>
+                                9 лет опыта работы в АНКОРе</div>
                         </li>
                     </ul>
                 </div>
             </div>
+            <h1>Основной состав</h1>
             <div class="team-wrapper">
                 <div class="jcarousel">
                     <ul class="team">
                         <li class="member">
                             <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <div class="description">
+                                <div class="football-position">Капитан команды</div>
+                                <div class="name">Алексей Миронов</div>
+                                <div class="position">Руководитель управления
+                                    всеми возможными управлениями, Москва</div>
+                                </div>
                         </li>
                         <li class="member">
                             <a href=""><img src="img/member.jpg"></a>
@@ -182,42 +230,6 @@
                 <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
                 <a href="#" class="jcarousel-control-next">&rsaquo;</a>
             </div>
-            <ul class="docs">
-                <li><a href="">Стандарты работы</a></li>
-                <li>|</li>
-                <li><a href="">Подбор персонала</a></li>
-                <li>|</li>
-                <li><a href="">Аутсорсинг процессов</a></li>
-                <li>|</li>
-                <li><a href="">Документооборот</a></li>
-                <li>|</li>
-                <li><a href="">Горячая линия</a></li>
-            </ul>
-            <div class="text">
-                <h2>Перспектива есть</h2>
-                <p>На ярмарке случилось странное происшествие: все наполнилось слухом, что где-то между товаром показалась красная свитка. Старухе, продававшей бублики, почудился сатана в образине свиньи, который беспрестанно наклонялся над возами, как будто искал чего. Это быстро разнеслось по всем углам уже утихнувшего табора; и все считали преступлением не верить... К этому присоединились еще увеличенные вести о чуде, виденном волостным писарем в развалившемся сарае, так что к ночи все теснее жались друг к другу; спокойствие разрушилось, и страх мешал всякому сомкнуть глаза свои; а те, которые были не совсем храброго десятка и запаслись ночлегами в избах, убрались домой. К числу последних принадлежал и Черевик с кумом и дочкою, которые вместе с напросившимися к ним в хату гостьми произвели сильный стук, так перепугавший нашу Хиврю. Кума уже немного поразобрало. Это можно было видеть из того, что он два раза проехал с своим возом по двору, покамест нашел хату. Гости тоже были в веселом расположении духа и без церемонии вошли прежде самого хозяина.</p>
-            </div>
-            <div class="graph">
-                <img src="img/infographic.jpg">
-            </div>
-            <div class="video">
-                <div class="item">
-                    Randstad Award-2015
-                    <iframe width="100%" height="150" src="https://www.youtube.com/embed/7zrjEcNUT8M" frameborder="0" allowfullscreen></iframe>
-                </div>
-                <div class="item">
-                    Randstad Award-2015
-                    <iframe width="100%" height="150" src="https://www.youtube.com/embed/7zrjEcNUT8M" frameborder="0" allowfullscreen></iframe>
-                </div>
-                <div class="item">
-                    Randstad Award-2015
-                    <iframe width="100%" height="150" src="https://www.youtube.com/embed/7zrjEcNUT8M" frameborder="0" allowfullscreen></iframe>
-                </div>
-                <div class="item">
-                    Randstad Award-2015
-                    <iframe width="100%" height="150" src="https://www.youtube.com/embed/7zrjEcNUT8M" frameborder="0" allowfullscreen></iframe>
-                </div>
-            </div>
         </div>
     </div>
     <div class="modal hidden" id="biography">
@@ -226,8 +238,8 @@
             <div class="row">
                 <div class="col col-1">
                     <div class="photo"><img src=""></div>
-                    <div class="phone">+7 999 888 77 66</div>
-                    <div class="email"><a href="mailto:info@ancor.ru">info@anor.ru</a></div>
+                    <div class="phone"></div>
+                    <div class="email"><a href=""></a></div>
                 </div>
                 <div class="col col-2">
                     <div class="name"></div>
