3bc069ddc5a269d1c4cf3777993df8be202af85b
Roman Alyakritskiy <rawork@yandex.ru>
2015-11-14 21:02:45 +0300

    fix main bg & people add

--- football.php
+++ football.php
@@ -6,7 +6,7 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="icon" type="image/png" href="img/football.png" />
     <link type="text/css" rel="stylesheet" href="css/jcarousel.css?2015111204" media="screen">
-    <link type="text/css" rel="stylesheet" href="css/style.css?2015111401" media="screen">
+    <link type="text/css" rel="stylesheet" href="css/style.css?2015111402" media="screen">
 </head>
 <body>
     <div class="container">
@@ -82,7 +82,7 @@
                                 10 лет опыта работы в АНКОРе</div>
                         </li>
                         <li class="member">
-                            <img src="img/member.jpg">
+                            <img src="img/people/Teplouhova.jpg">
                             <div class="football-position">Капитан команды</div>
                             <div class="name">Анастасия Теплоухова</div>
                             <div class="position">Исполнительный директор ANCOR Professional<br>
@@ -96,133 +96,203 @@
                 <div class="jcarousel">
                     <ul class="team">
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="description">
-                                <div class="football-position">Капитан команды</div>
-                                <div class="name">Алексей Миронов</div>
-                                <div class="position">Руководитель управления
-                                    всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Skvortsova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Александра Скворцова</div>
+                                    <div class="position">Руководитель групп подбора персонала «Административный менеджмент, HR и юриспруденция», «Информационные технологии», Москва<br>
+                                        16 лет опыта работы в АНКОРе</div>
                                 </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Gorshkova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Анна Горшкова</div>
+                                    <div class="position">Региональный менеджер направления «Логистика», Москва<br>
+                                        5 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Kukalets.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Наталья Кукалец</div>
+                                    <div class="position">Руководитель группы подбора персонала «Маркетинг, реклама и PR», Москва<br>
+                                        15 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Sur.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Олеся Сур</div>
+                                    <div class="position">Старший консультант группы «Финансы и бухгалтерия», Москва<br>
+                                        12 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Bobyreva.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Анна Бобырева</div>
+                                    <div class="position">Консультант группы «Информационные технологии», Москва<br>
+                                        9 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Orlova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Вера Орлова</div>
+                                    <div class="position">Консультант группы «Маркетинг, реклама и PR», Москва<br>
+                                        9 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Larina.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Светлана Ларина</div>
+                                    <div class="position">Консультант группы «Административный менеджмент, HR и юриспруденция», Москва<br>
+                                        8 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Kotenko.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Елизавета Котенко</div>
+                                    <div class="position">Руководитель группы подбора персонала «Логистика», Санкт-Петербург<br>
+                                        Опыт работы в АНКОРе 8 лет</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Panykova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Наталья Панькова</div>
+                                    <div class="position">Руководитель группы подбора персонала «Поддержка бизнеса», Санкт-Петербург<br>
+                                        4 года опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Vasilyeva.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Александра Васильева</div>
+                                    <div class="position">Консультант группы подбора персонала «Поддержка бизнеса», Санкт-Петербург</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Mayorova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Ксения Гатенюк</div>
+                                    <div class="position">Консультант по подбору персонала, Калининград<br>
+                                        4 года опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Karmaeva.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Ольга Кармаева</div>
+                                    <div class="position">Руководитель группы подбора персонала «Поддержка бизнеса», Нижний Новгород<br>
+                                        11 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Aitova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Галина Аитова</div>
+                                    <div class="position">Руководитель группы подбора персонала, Самара<br>
+                                        7 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Baranova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Ольга Баранова</div>
+                                    <div class="position">Младший консультант по подбору персонала, Казань</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Skripchenko.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Надежда Скрипченко</div>
+                                    <div class="position">Старший консультант по подбору персонала, Ростов-на-Дону<br>
+                                        6 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Evseeva.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Анастасия Евсеева</div>
+                                    <div class="position">Рекрутер, Волгоград</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Trofimova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Ольга Трофимова</div>
+                                    <div class="position">Консультант по подбору персонала, Екатеринбург<br>
+                                        8 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Plesovskih.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Анна Плесовских</div>
+                                    <div class="position">Директор по маркетинговым коммуникациям, Москва<br>
+                                        7 лет опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Feoktistova.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Светлана Феоктистова
+                                        </div>
+                                    <div class="position">Специалист по маркетинговым коммуникациям, Москва<br>
+                                        4 года опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                         <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
-                        </li>
-                        <li class="member">
-                            <a href=""><img src="img/member.jpg"></a>
-                            <div class="name">Алексей Миронов</div>
-                            <div class="position">Руководитель управления
-                                всеми возможными управлениями, Москва</div>
+                            <a href=""><img src="img/people/Kovalenko.jpg"></a>
+                            <div class="relative">
+                                <div class="description">
+                                    <div class="name">Наталья Коваленко
+                                        </div>
+                                    <div class="position">Региональный специалист по маркетингу, Санкт-Петербург<br>
+                                        3 года опыта работы в АНКОРе</div>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
